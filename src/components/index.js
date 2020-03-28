9c60292d3775e809577d3abc931a39ffca5aca60
motokikasai <kmomoky@hotmail.com>
2020-03-28 14:38:58 +0100

    Structure the project with components, styles and assets diretories & add lorempicsum url to avatar component

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Avatar() {
+  return (
+    <section>
+      <h1>hello world</h1>
+      <div className="source">
+        <img src="https://picsum.photos/400/400" alt="from lorempicsum" />
+      </div>
+    </section>
+  );
+}
+
+export default Avatar;
