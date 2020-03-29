bfa01ec91971a01a0d68d88a49a712301038c8e3
motokikasai <kmomoky@hotmail.com>
2020-03-30 00:53:40 +0200

    Add hover effects on theme changes via EventListeners

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -17,6 +17,32 @@ function Theme() {
           checked={check}
           onChange={() => {
             const bd = document.body;
+            const card = document.querySelector(".card");
+
+            function darkEnter(e) {
+              e.target.style.boxShadow =
+                "-3px -3px 5px rgba(120, 120, 120, 0.7), 3px 3px 5px rgb(20, 20, 20)";
+              document.querySelector("img.shade").style.boxShadow =
+                "-3px -3px 5px rgba(120, 120, 120, 0.7), 3px 3px 5px rgb(20, 20, 20)";
+            }
+            function darkLeave(e) {
+              e.target.style.boxShadow =
+                "-6px -6px 12px rgba(120, 120, 120, 0.7), 9px 9px 16px rgba(20, 20, 20, 0.7)";
+              document.querySelector("img.shade").style.boxShadow =
+                "-6px -6px 12px rgba(120, 120, 120, 0.7), 9px 9px 16px rgba(20, 20, 20, 0.7)";
+            }
+            function lightEnter(e) {
+              e.target.style.boxShadow =
+                "-3px -3px 9px rgb(255, 255, 255), 3px 3px 9px rgb(151, 151, 151)";
+              document.querySelector("img.shade").style.boxShadow =
+                "-3px -3px 9px rgb(255, 255, 255), 3px 3px 9px rgb(151, 151, 151)";
+            }
+            function lightLeave(e) {
+              e.target.style.boxShadow =
+                "-9px -9px 16px rgba(255, 255, 255, 0.7), 9px 9px 16px rgba(151, 151, 151, 0.7)";
+              document.querySelector("img.shade").style.boxShadow =
+                "-9px -9px 16px rgba(255, 255, 255, 0.7), 9px 9px 16px rgba(151, 151, 151, 0.7)";
+            }
 
             if (check) {
               bd.style.background = "#323232";
@@ -24,13 +50,31 @@ function Theme() {
               document.querySelector(".card").style.boxShadow =
                 "-6px -6px 12px rgba(120, 120, 120, 0.7), 9px 9px 16px rgba(20, 20, 20, 0.7)";
               document.querySelector("img.shade").style.boxShadow =
-                "-5px -5px 12px rgba(120, 120, 120, 0.7), 9px 9px 16px rgba(20, 20, 20, 0.9)";
+                "-6px -6px 12px rgba(120, 120, 120, 0.7), 9px 9px 16px rgba(20, 20, 20, 0.7)";
+
+              // RemoveEventListener
+              card.removeEventListener("mouseenter", lightEnter);
+              card.removeEventListener("mouseleave", lightLeave);
+
+              // AddEventListener
+              card.addEventListener("mouseenter", darkEnter);
+              card.addEventListener("mouseleave", darkLeave);
+
               document.querySelector(".cover").classList.add("cover-dark");
             } else {
               bd.style.background = "#efeeee";
               bd.style.color = "rgb(63, 63, 63)";
               document.querySelector(".card").style.boxShadow =
                 "-9px -9px 16px rgba(255, 255, 255, 0.7), 9px 9px 16px rgba(151, 151, 151, 0.7)";
+
+              // RemoveEventListener
+              card.removeEventListener("mouseenter", lightEnter);
+              card.removeEventListener("mouseleave", lightLeave);
+
+              // AddEventListener
+              card.addEventListener("mouseenter", lightEnter);
+              card.addEventListener("mouseleave", lightLeave);
+
               document.querySelector("img.shade").style.boxShadow =
                 "-9px -9px 16px rgba(255, 255, 255, 0.7), 9px 9px 16px rgba(151, 151, 151, 0.7)";
               document.querySelector(".cover").classList.remove("cover-dark");
